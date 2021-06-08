95818ba507c36c3318f57ed3c88817e8bd3c6ccf
sangbooom <tkdqja4164@naver.com>
2021-06-08 20:35:09 +0900

    add: backUrl 엔드포인트 Config에 담아서 보여주기

--- front/components/ImagesZoom/index.js
+++ front/components/ImagesZoom/index.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import Slick from 'react-slick';
+import { backUrl } from '../config/config';
 
 import { Overlay, Global, Header, CloseBtn, ImgWrapper, Indicator, SlickWrapper } from './styles';
 
@@ -25,7 +26,7 @@ const ImagesZoom = ({ images, onClose }) => {
           >
             {images.map((v) => (
               <ImgWrapper key={v.src}>
-                <img src={`http://localhost:3065/${v.src}`} alt={v.src} />
+                <img src={`${backUrl}/${v.src}`} alt={v.src} />
               </ImgWrapper>
             ))}
           </Slick>
--- front/config/config.js
+++ front/config/config.js
@@ -1,1 +1,1 @@
-exports.backUrl = 'http://localhost:3065';
+export const backUrl = 'http://3.35.234.85/';
